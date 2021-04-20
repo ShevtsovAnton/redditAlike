f6350afe46aadefac9047fd7058d6240f4b14816
Anton <a0660576041@gmail.com>
2021-04-20 21:46:01 +0300

    feat: add login, logout functionality

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,19 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import trim from './middleware/trim';
 import authRoutes from './routes/auth';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
+app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('Hello World'));
 app.use('/api/auth', authRoutes);
