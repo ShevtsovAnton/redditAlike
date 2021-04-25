87a3debb3fc5575f4489a407097927beb9dde0d2
Anton <a0660576041@gmail.com>
2021-04-25 16:26:31 +0300

    feat: add signup, login pages

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,6 +6,13 @@ import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
 import auth from '../middleware/auth';
 
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -22,9 +29,9 @@ const register = async (req: Request, res: Response) => {
     //TODO: create the user
     const user = new User({ email, username, password });
     errors = await validate(user);
-    console.log('ERRORS', errors);
+
     if (errors.length > 0) {
-      return res.status(400).json({ errors });
+      return res.status(400).json(mapErrors(errors));
     }
     await user.save();
 
@@ -49,7 +56,7 @@ const login = async (req: Request, res: Response) => {
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ username: 'User not found' });
     }
 
     const passwordMatches = await bcrypt.compare(password, user.password);
